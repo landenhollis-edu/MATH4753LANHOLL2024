684041dbc9405bef9691357cf581298bc9df1a99
landenhollis-edu <142827312+landenhollis-edu@users.noreply.github.com>
2024-09-18 13:08:26 -0500

    Initial commit

---
+++ R/myplot.R
@@ -0,0 +1,17 @@
+#' The plot function
+#'
+#' @param x a vector of quantitative data
+#' @param y a vector of quantitative data
+#'
+#' @return a plot
+#'
+#' @examples
+#' myplot(x = 1.10, y = 2.5)
+#'
+#' @export
+
+myplot <- function(x,y){
+
+dataplotted <- plot(x,y)
+
+}
